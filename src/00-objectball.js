3fb2b8a82993c8b71c06bc986d50e0c7e149b4fb
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-21 18:21:53 -0500

    commit tree bb3542492b753f194e9c6aa9e3bf77173746752e

---
+++ src/00-objectball.js
@@ -0,0 +1,20 @@
+function gameObject {
+    side {
+        home: ,
+        away:,
+    }
+    details {
+        teamName: string,
+        colors: arrayofStrings,
+        players: {
+            number: ,
+            shoe: ,
+            points: ,
+            rebounds: ,
+            assists: ,
+            steals: ,
+            blocks: ,
+            slamDunks: ,
+        }
+    }
+}
